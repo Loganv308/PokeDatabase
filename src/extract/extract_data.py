e844e03ff868bd1260709b68f1ed3fde87f4ede5
loganv308 <loganv308@gmail.com>
2026-02-23 20:02:18 -0600

    Refactor extract_data.py to improve file handling and add file size check
    - Added a function to get the file size of the JSON file.
    - Modified the logic to check if the JSON file exists and its size before writing new data.
    - Improved error handling for file not found scenarios.
    - Updated the file path to use a consistent format.

--- src/extract/extract_data.py
+++ src/extract/extract_data.py
@@ -1,14 +1,23 @@
 import pandas as pd
+import requests
 import numpy
 import json
-import requests
+import os
 
 API_URL = "https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0"
+FILEPATH = "src\data\pokemon_data.json"
 
 def getAPIData():
     response = requests.get(API_URL)
 
     return response
+
+def getFileSize(filePath: str) -> int:
+
+    fileSize = os.path.getsize(filePath)
+
+    return fileSize
+
 
 if __name__ == "__main__":
     apiData = getAPIData()
@@ -19,17 +28,27 @@ if __name__ == "__main__":
         # Takes the response in as JSON.
         pokemonData = apiData.json()
 
-        # Creates the .json locally, defined as 'f'.
-        with open("src\data\pokemon_data.json", 'w') as f:
-            if(f):
-                print("File already exists, moving on...")
+        try:
+            jsonFileSize = getFileSize(FILEPATH)
+
+            if(jsonFileSize == 0):
+                # Creates the .json locally, defined as 'f'.
+                with open(jsonFileSize, 'w') as f:
+                    # Adding in the "indent" and "ensure_ascii" gives a pretty print to the dumped JSON.
+                    json.dump(pokemonData, f, ensure_ascii=False, indent=4)
             else:
-                # Adding in the "indent" and "ensure_ascii" gives a pretty print to the dumped JSON.
-                json.dump(pokemonData, f, ensure_ascii=False, indent=4)
+                print("File already exists and contains data, moving on...")
 
-        pokemon_df = pd.json_normalize(pokemonData)
+            pokemonDf = pd.json_normalize(pokemonData["results"])
+            
+            # Takes the index # and puts it at 1 instead of 0. This correlates better with the official PokeAPI
+            pokemonDf.index = range(1, len(pokemonDf) + 1)
 
-        print(pokemon_df)
+            print(pokemonDf.head())
+
+        except FileNotFoundError as e:
+            print("File not found...")
+
     else:
         # Prints any errors that may occur
         print("Error: " + str(apiData.status_code))
